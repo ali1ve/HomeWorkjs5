bcfa35fecfdaf7779d479b4c7bff237e55ac695f
ali1ve <aliaga.yahya@gmail.com>
2024-10-31 01:54:26 +0400

    homework5

---
+++ app.js
@@ -0,0 +1,11 @@
+let user = prompt("Please write something")
+
+console.log(user.length);
+console.log(user.includes("e"));
+console.log(user.startsWith("code"));
+console.log(user.endsWith("code"));
+console.log(user.replaceAll("t","####"));
+
+
+
+
